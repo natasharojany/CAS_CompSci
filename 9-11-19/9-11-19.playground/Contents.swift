5eada8dad391f239eddd91553a9a4eaa472fd6c8
Lance Davenport <lancedavenport20@k12.xrds.org>
2019-09-11 10:10:58 -0700

    New Work

---
+++ 9-11-19/9-11-19.playground/Contents.swift
@@ -0,0 +1,28 @@
+func square(math: Int) -> Int {
+    let number = math * math
+    return (number)
+}
+
+print(square(math: 5))
+
+func five() -> Int {
+    return 5
+}
+print(five())
+
+func fullName(first: String, last: String) -> String {
+    return first + " " + last
+}
+print(fullName(first: "Lance", last: "Davenport"))
+
+func shout(input: String) -> String {
+    return input + "!"
+}
+print(shout(input: "Money"))
+
+func numberName(num: Int) -> String {
+//    let singleDigits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let letters = ["zero","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    return letters[num]
+}
+print(numberName(num: 5))
