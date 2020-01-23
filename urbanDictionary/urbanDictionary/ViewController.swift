eb8998957700bfe127ca14a891fdf7278ee54728
lancedavenport <48220214+lancedavenport@users.noreply.github.com>
2020-01-23 09:31:15 -0800

    Add files via upload

--- urbanDictionary/urbanDictionary/ViewController.swift
+++ urbanDictionary/urbanDictionary/ViewController.swift
@@ -15,12 +15,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var posField: UITextField!
     @IBOutlet weak var defView: UITextView!
     
-    //List and add stuff to list
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+   
     func fileUrl() -> URL {
         let documentURL = try!
             FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
@@ -28,20 +30,29 @@ class ViewController: UIViewController {
     }
     
     @IBAction func addWords(_ sender: UIButton) {
-        if let oldWords:[[String : String]] = getJsonData() as [[String:String]]? {
-            if let oldJson = try? JSONSerialization.data(withJSONObject: oldWords, options: []) {
-                // Add old words to JSON file
-            }
-        }
         let data: [String:String] = [
             "Name": nameField.text ?? "N/A",
             "Part of Speech": posField.text ?? "N/A",
             "Definition": defView.text ?? "N/A"
-            
         ]
+        
+        var wordstoSave: [[String:String]] = []
+        if var oldWords:[[String : String]] = getJsonData() as [[String:String]]? {
+            oldWords.append(data)
+            wordstoSave = oldWords
+        } else {
+            wordstoSave = [data]
+        }
+        
+    
+        let anotherSorted = wordstoSave.sorted(by: {
+            (a, b) in
+            return a["Name"]! < b["Name"]!
+        })
+        
         let url = fileUrl()
-        if let jsonData = try? JSONSerialization.data(withJSONObject: data, options: []) {
-            // Append data into JSON file
+        if let jsonData = try? JSONSerialization.data(withJSONObject: anotherSorted, options: []) {
+            try? jsonData.write(to: url)
             print(data)
             nameField.text = ""
             defView.text = ""
@@ -53,36 +64,43 @@ class ViewController: UIViewController {
     
     func getJsonData() -> [[String:String]]? {
         let url = fileUrl()
-        let responseData: Data? = try! Data(contentsOf: url)
+        let responseData: Data? = try? Data(contentsOf: url)
         if let responseData = responseData {
-            let json: String? = try? JSONSerialization.jsonObject(with: responseData, options: []) as? String
-            if let dictionary: [[String:String]]? = json as? [[String:String]]? {
+            let json = try? JSONSerialization.jsonObject(with: responseData, options: [])
+            if let dictionary: [[String:String]] = json as? [[String:String]] {
                 return dictionary
             }
         }
-    } // Missing return in a function expected to return '[[String : String]]?' error
+        return nil
+    }
     
-    
-    @IBAction func loadData(_ sender: UIButton) {
-        let url = fileUrl()
-        let responseData: Data? = try! Data(contentsOf: url)
-        if let responseData = responseData {
-            let json: Any? = try? JSONSerialization.jsonObject(with: responseData, options: [])
-            if let json = json {
-                let dictionary: [String: Any]? = json as? [String: Any]
-                if let dictionary = dictionary {
-                    for names in dictionary {
-                        let name: String = dictionary["Name"] as! String
-                        let definition: String = dictionary["Definition"] as! String
-                        let pos: String = dictionary["Part of Speech"] as! String
-                        print(name, definition, pos)
-                        textView.text = ("Name: \(name) (\(pos))\n Definition: \(definition)\n ")
-                    }
-                }
+    func allData() {
+        textView.text = ("")
+        let dictionary = getJsonData()
+        if let dictionary = dictionary {
+            for wordEntry in dictionary {
+                let name: String = wordEntry["Name"]!
+                let definition: String = wordEntry["Definition"]!
+                let pos: String = wordEntry["Part of Speech"]!
+                print(name, definition, pos)
+                textView.text = textView.text! + "\nName: \(name)(\(pos))\n Defintion: \(definition)\n"
             }
         }
     }
     
+    @IBAction func loadData(_ sender: UIButton) {
+        textView.text = ("")
+        let dictionary = getJsonData()
+        if let dictionary = dictionary {
+            for wordEntry in dictionary {
+                let name: String = wordEntry["Name"]!
+                let definition: String = wordEntry["Definition"]!
+                let pos: String = wordEntry["Part of Speech"]!
+                print(name, definition, pos)
+                textView.text = textView.text! + "\nName: \(name)(\(pos))\n Defintion: \(definition)\n"
+            }
+        }
+    }
     
     
     @IBAction func clearData(_ sender: UIButton) {
