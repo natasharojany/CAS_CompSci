b5127e21cc13428aa3713eb27db198b1d4d29a61
lancedavenport <48220214+lancedavenport@users.noreply.github.com>
2020-01-30 09:18:50 -0800

    Add files via upload

---
+++ layoutExercise/layoutExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  layoutExercise
+//
+//  Created by Lance Davenport on 1/29/20.
+//  Copyright © 2020 Lance Davenport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
