2cbbc46a0327a072318fa26f64c51d8bda540dfe
lancedavenport <48220214+lancedavenport@users.noreply.github.com>
2020-02-19 09:06:38 -0800

    Add files via upload

--- numberGuesser/numberGuesser/numberViewController.swift
+++ numberGuesser/numberGuesser/numberViewController.swift
@@ -22,16 +22,55 @@ class numberViewController: UIViewController {
     }
     func checkDifficulty() -> Int{
         if difficulty == "easy" {
-            return 15
+            let random = Int.random(in: 5...15)
+            return random
         }
         else if difficulty == "medium"{
-            return 45
+            let random = Int.random(in: 45...60)
+            return random
         }
         else{
-            return 105
+            let random = Int.random(in: 90...250)
+            return random
         }
     }
     
+    
+    
+    @objc func buttonAction() {
+        let trueDifficulty = checkDifficulty()
+        let number = Int.random(in: 1...trueDifficulty)
+        let buttons = (1...trueDifficulty).map({ (x) -> UIButton in
+            let newButton = UIButton()
+            newButton.setTitle("Button \(x)", for: .normal)
+            newButton.frame = CGRect(x: 0, y: 0, width: 400, height: 25)
+            newButton.backgroundColor = UIColor(displayP3Red: 0.3, green: 0.5, blue: 0.6, alpha: 1)
+            if x > number {
+                newButton.addTarget(self, action: #selector(tooHigh), for: .touchUpInside)
+            }
+            else if x < number {
+                newButton.addTarget(self, action: #selector(tooLow), for: .touchUpInside)
+            }
+            else{
+                newButton.addTarget(self, action: #selector(justRight), for: .touchUpInside)
+            }
+            return newButton
+        })
+        stackView.frame = CGRect(x: 0, y: 0, width: 400, height: trueDifficulty*45)
+        scrollView.contentSize = CGSize(width: 400, height: trueDifficulty*45)
+        stackView.backgroundColor = UIColor(displayP3Red: 0.3, green: 0.5, blue: 0.6, alpha: 1)
+        scrollView.backgroundColor = UIColor(displayP3Red: 0.3, green: 0.5, blue: 0.6, alpha: 1)
+        for button in buttons{
+            stackView.addArrangedSubview(button)
+            let constraint = NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal, toItem: buttons[0], attribute: .height, multiplier: 1, constant: 0)
+            
+            if button != buttons[0] {
+                stackView.addConstraint(constraint)
+                
+            }
+            
+        }
+    }
     @objc func tooLow(){
         let alert = UIAlertController(title: "Try again!", message: "Too low!", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default))
@@ -43,44 +82,10 @@ class numberViewController: UIViewController {
         alert.addAction(UIAlertAction(title: "Ok", style: .default))
         self.present(alert, animated: true, completion: {})
     }
-   
+    
     @objc func justRight(){
         let alert = UIAlertController(title: "Congrats! You got it!", message: "Go back and try a harder level.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Ok", style: .default))
         self.present(alert, animated: true, completion: {})
-    }
-   
-    @objc func buttonAction() {
-        var number = Int.random(in: 1...checkDifficulty())
-        stackView.frame = CGRect(x: 0, y: 0, width: 400, height: checkDifficulty()*45)
-        scrollView.contentSize = CGSize(width: 400, height: checkDifficulty()*45)
-        stackView.backgroundColor = UIColor(displayP3Red: 0.3, green: 0.5, blue: 0.6, alpha: 1)
-        scrollView.backgroundColor = UIColor(displayP3Red: 0.3, green: 0.5, blue: 0.6, alpha: 1)
-        let buttons = (1...checkDifficulty()).map({ (x) -> UIButton in
-            let newButton = UIButton()
-            newButton.setTitle("Button \(x)", for: .normal)
-            newButton.frame = CGRect(x: 0, y: 0, width: 400, height: 25)
-            newButton.backgroundColor = UIColor(displayP3Red: 0.3, green: 0.5, blue: 0.6, alpha: 1)
-            if x < number {
-                newButton.addTarget(self, action: #selector(tooLow), for: .touchUpInside)
-            }
-            else if x > number {
-                newButton.addTarget(self, action: #selector(tooHigh), for: .touchUpInside)
-            }
-            else{
-                newButton.addTarget(self, action: #selector(justRight), for: .touchUpInside)
-            }
-            return newButton
-        })
-        for button in buttons{
-            stackView.addArrangedSubview(button)
-            let constraint = NSLayoutConstraint(item: button, attribute: .height, relatedBy: .equal, toItem: buttons[0], attribute: .height, multiplier: 1, constant: 0)
-            
-            if button != buttons[0] {
-                stackView.addConstraint(constraint)
-                
-            }
-            
-        }
     }
 }
