27da18039dfb95e93705f06ade01dd32d187891d
lancedavenport <48220214+lancedavenport@users.noreply.github.com>
2020-02-19 09:10:50 -0800

    Add files via upload

---
+++ numberGuesser/numberGuesser/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  numberGuesser
+//
+//  Created by Lance Davenport on 2/18/20.
+//  Copyright © 2020 Lance Davenport. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var easyButton: UIButton!
+    @IBOutlet weak var mediumButton: UIButton!
+    @IBOutlet weak var hardButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       if let  guessingVC = segue.destination as? numberViewController, let sender = sender as? UIButton  {
+            if sender == easyButton {
+                guessingVC.difficulty = "easy"
+            }
+            if sender == mediumButton {
+                guessingVC.difficulty = "medium"
+            }
+            if sender == hardButton {
+                guessingVC.difficulty = "hard"
+            }
+            
+            
+        }
+        
+    }
+    
+}
+
