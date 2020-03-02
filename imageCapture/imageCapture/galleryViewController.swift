bf69f898e03e0330120f79ba227c0097e3c80ae9
lancedavenport <48220214+lancedavenport@users.noreply.github.com>
2020-03-01 18:50:06 -0800

    Add files via upload

--- imageCapture/imageCapture/galleryViewController.swift
+++ imageCapture/imageCapture/galleryViewController.swift
@@ -20,21 +20,21 @@ class galleryViewController: UIViewController {
         loadPhotos()
         // Do any additional setup after loading the view.
     }
-    @IBAction func captionButton(_ sender: UIButton) {
-        return 
-    }
+
     
     func loadPhotos(){
         let pictures = images.map({(image) -> UIImageView in
             let newImageView = UIImageView()
             newImageView.image = image
-            newImageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+            newImageView.frame = CGRect(x: 0, y: 0, width: 400, height: 600)
             return newImageView
         })
+        let count = images.count
+        stackView.frame = CGRect(x: 0, y: 0, width: 400, height: count*600)
+        scrollView.contentSize = CGSize(width: 400, height: count*600)
         for (_, image) in pictures.enumerated() {
             stackView.addArrangedSubview(image)
             let constraint = NSLayoutConstraint(item: image, attribute: .height, relatedBy: .equal, toItem: pictures[0], attribute: .height, multiplier: 1, constant: 1)
-            stackView.addArrangedSubview(image)
             if image != pictures[0] {
                 stackView.addConstraint(constraint)
             }
